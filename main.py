d0cfd592f2b445e3a87433ed57e82c7336882c41
augustoifal <augusto.melo@ifal.edu.br>
2020-04-28 19:20:33 +0000

    operações aritméticas básicas

---
+++ main.py
@@ -0,0 +1,14 @@
+# Aula 1 - Escrevendo na tela
+print("Olá Mundo!")# a função print("texto"), exibe "texto" na tela
+# atribuíndo uma variável numérica x
+x = 0 # x é um valor inteiro 0
+print(x) # exibe o valor de x
+# operações aritméticas, uso de operadores aritméticos
+x = 5 + 3 # adição
+print(x) # exibe o valor de x
+x = 5 - 3 # substração
+print(x) # exibe o valor de x
+x = 5 * 3 # multiplicação
+print(x) # exibe o valor de x
+x = 15 / 3 # divisão
+print(x) # exibe o valor de x
